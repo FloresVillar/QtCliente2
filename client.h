2f1de9fdd777b83c9a399133a4aca2b5a0b5aa47
flores <efloresv@uni.pe>
2021-10-26 20:37:59 -0500

    version incial corregidaxd

---
+++ client.h
@@ -0,0 +1,28 @@
+#ifndef CLIENT_H
+#define CLIENT_H
+
+#include <QWidget>
+#include<QTcpSocket>
+#include<QDebug>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class Client; }
+QT_END_NAMESPACE
+
+class Client : public QWidget
+{
+    Q_OBJECT
+
+public:
+    Client(QWidget *parent = nullptr);
+    ~Client();
+private slots:
+    void requesMessage();
+    void showError();
+    void readMessage();
+
+private:
+    Ui::Client *ui;
+    QTcpSocket *clientSocket = nullptr;
+};
+#endif // CLIENT_H
